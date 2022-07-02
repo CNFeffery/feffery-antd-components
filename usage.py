30906d7c5ec275529a77d782c948f794885a2810
CNFeffery <fefferypzy@gmail.com>
2022-07-02 17:34:59 +0800

    test: usage.py新增自定义骨骼屏demo

--- usage.py
+++ usage.py
@@ -7,6 +7,9 @@ from dash import html
 from requests import options
 import feffery_antd_components as fac
 from dash.dependencies import Input, Output, ALL
+from faker import Faker
+
+faker = Faker(locale='zh_CN')
 
 app = dash.Dash(
     __name__
@@ -18,59 +21,93 @@ app.layout = html.Div(
         fac.AntdSpace(
             [
                 fac.AntdButton(
-                    '触发2秒骨骼屏动画', id='skeleton-custom-demo-input',
+                    '触发2秒自定义骨骼屏动画', id='skeleton-custom-demo-input',
                     type='primary'
                 ),
 
                 fac.AntdCustomSkeleton(
-                    fac.AntdText(
-                        'nClicks: 0', id='skeleton-custom-demo-output', strong=True),
-                    skeletonContent=fac.AntdSpace(
+                    fac.AntdTable(
+                        columns=[
+                            {
+                                'title': '国家名示例',
+                                'dataIndex': '国家名示例',
+                                'width': '25%'
+                            },
+                            {
+                                'title': '省份名示例',
+                                'dataIndex': '省份名示例',
+                                'width': '25%'
+                            },
+                            {
+                                'title': '城市名示例',
+                                'dataIndex': '城市名示例',
+                                'width': '25%'
+                            },
+                            {
+                                'title': '日期示例',
+                                'dataIndex': '日期示例',
+                                'width': '25%'
+                            }
+                        ],
+                        bordered=True,
+                        data=[
+                            {
+                                'key': i,
+                                '国家名示例': faker.country(),
+                                '省份名示例': faker.province(),
+                                '城市名示例': faker.city_name(),
+                                '日期示例': faker.date(pattern="%Y-%m-%d", end_datetime=None)
+                            }
+                            for i in range(3)
+                        ]
+                    ),
+                    id='custom-skeleton-demo',
+                    skeletonContent=html.Div(
                         [
                             fac.AntdRow(
                                 [
                                     fac.AntdCol(
-                                        fac.AntdSkeletonAvatar(
-                                            active=True,
-                                            size='large'
+                                        fac.AntdSkeletonButton(
+                                            block=True,
+                                            active=True
                                         ),
-                                        flex='none'
+                                        span=6,
+                                        style={
+                                            'padding': '4px'
+                                        }
+                                    )
+                                ] * 16
+                            ),
+                            fac.AntdSpace(
+                                [
+                                    html.Div(
+                                        fac.AntdSkeletonButton(
+                                            active=True,
+                                            size='small',
+                                            block=True
+                                        ),
+                                        style={
+                                            'width': '80px'
+                                        }
                                     ),
-
-                                    *[
-                                        fac.AntdCol(
-                                            fac.AntdSkeletonButton(
-                                                block=True,
-                                                active=True,
-                                                size='small'
-                                            ),
-                                            flex='auto'
-                                        )
-                                    ] * 10
+                                    html.Div(
+                                        fac.AntdSkeletonButton(
+                                            active=True,
+                                            size='small',
+                                            block=True
+                                        ),
+                                        style={
+                                            'width': '60px'
+                                        }
+                                    )
                                 ],
-                                align='middle',
-                                gutter=10
-                            ),
-                            fac.AntdSkeletonButton(
-                                block=True,
-                                active=True,
-                                size='small'
-                            ),
-                            fac.AntdSkeletonButton(
-                                block=True,
-                                active=True,
-                                size='small'
-                            ),
-                            fac.AntdSkeletonButton(
-                                block=True,
-                                active=True,
-                                size='small'
+                                style={
+                                    'float': 'right',
+                                    'paddingRight': '4px',
+                                    'paddingTop': '15px'
+                                }
                             )
-                        ],
-                        direction='vertical',
-                        style={
-                            'width': '100%'
-                        }
+                        ]
                     )
                 ),
             ],
@@ -190,15 +227,49 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output('skeleton-custom-demo-output', 'children'),
+    Output('custom-skeleton-demo', 'children'),
     Input('skeleton-custom-demo-input', 'nClicks'),
     prevent_initial_call=True
 )
 def skeleton_custom_callback_demo(nClicks):
     import time
-    time.sleep(20)
+    time.sleep(2)
 
-    return f'nClicks: {nClicks}'
+    return fac.AntdTable(
+        columns=[
+            {
+                'title': '国家名示例',
+                'dataIndex': '国家名示例',
+                'width': '25%'
+            },
+            {
+                'title': '省份名示例',
+                'dataIndex': '省份名示例',
+                'width': '25%'
+            },
+            {
+                'title': '城市名示例',
+                'dataIndex': '城市名示例',
+                'width': '25%'
+            },
+            {
+                'title': '日期示例',
+                'dataIndex': '日期示例',
+                'width': '25%'
+            }
+        ],
+        bordered=True,
+        data=[
+            {
+                'key': i,
+                '国家名示例': faker.country(),
+                '省份名示例': faker.province(),
+                '城市名示例': faker.city_name(),
+                '日期示例': faker.date(pattern="%Y-%m-%d", end_datetime=None)
+            }
+            for i in range(3)
+        ]
+    )
 
 
 if __name__ == '__main__':
