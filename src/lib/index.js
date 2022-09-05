386ca36563fa4ad1376783f5e77466099a78da85
CNFeffery <fefferypzy@gmail.com>
2022-09-05 19:38:31 +0800

    fix: 修复因警告信息过滤策略而导致dash开发者工具部分功能失效的问题

--- src/lib/index.js
+++ src/lib/index.js
@@ -98,16 +98,16 @@ import AntdPopupCard from './components/AntdPopupCard.react'
 目前已知无关警告信息：
 1. 数组推导形成的组件，每个子组件需要唯一的key
 */
-const backup = console.error;
-console.error = (msg) => {
-    const supressedWarnings = [
-        'Each child in a list should have a unique'
-    ];
+// const backup = console.error;
+// console.error = (msg) => {
+//     const supressedWarnings = [
+//         'Each child in a list should have a unique'
+//     ];
 
-    if (!supressedWarnings.some(entry => msg.includes(entry))) {
-        backup.apply(console, arguments);
-    }
-};
+//     if (!supressedWarnings.some(entry => msg.includes(entry))) {
+//         backup.apply(console, arguments);
+//     }
+// };
 
 export {
     AntdDatePicker,
