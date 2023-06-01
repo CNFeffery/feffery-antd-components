53d4b18b64485da0dce1fe61512ba84a11412edf
CNFeffery <fefferypzy@gmail.com>
2023-06-01 22:20:42 +0800

    doc: 为AntdButton补齐英文参数说明

--- src/jl/'feffery'_antdbutton.jl
+++ src/jl/'feffery'_antdbutton.jl
@@ -11,30 +11,31 @@ export 'feffery'_antdbutton
 An AntdButton component.
 
 Keyword arguments:
-- `children` (a list of or a singular dash component, string or number; optional)
-- `id` (String; optional)
-- `autoSpin` (Bool; optional)
-- `block` (Bool; optional)
-- `className` (String | Dict; optional)
-- `danger` (Bool; optional)
-- `debounceWait` (Real; optional)
-- `disabled` (Bool; optional)
-- `href` (String; optional)
-- `icon` (a list of or a singular dash component, string or number; optional)
-- `key` (String; optional)
-- `loading` (Bool; optional)
-- `loadingChildren` (a list of or a singular dash component, string or number; optional)
+- `children` (a list of or a singular dash component, string or number; optional): Children of current component
+- `id` (String; optional): Id of current component
+- `autoSpin` (Bool; optional): Setting whether to automatically update loading to true after each button click, 
+enabling callback operations with a non-clickable button effect; default is false
+- `block` (Bool; optional): Setting whether the button should span the full width of its parent element, default is false
+- `className` (String | Dict; optional): CSS class of current component
+- `danger` (Bool; optional): Setting whether the button should be in a dangerous state, default is false
+- `debounceWait` (Real; optional): Used to set the debounce waiting duration (in milliseconds) for nClicks listener update; default is 0
+- `disabled` (Bool; optional): Setting whether the button should be rendered as a disabled state, default is false
+- `href` (String; optional): Setting the URL for redirection, similar to the functionality of an <a> tag
+- `icon` (a list of or a singular dash component, string or number; optional): Component-specific parameter for setting the prefix icon element of the button
+- `key` (String; optional): Refreshing the key can force a redraw of the current component.
+- `loading` (Bool; optional): Used to set whether the button renders a "loading, not clickable" effect; default is false
+- `loadingChildren` (a list of or a singular dash component, string or number; optional): Children of current component while loading=true
 - `loading_state` (optional): . loading_state has the following type: lists containing elements 'is_loading', 'prop_name', 'component_name'.
 Those elements have the following types:
   - `is_loading` (Bool; optional): Determines if the component is loading or not
   - `prop_name` (String; optional): Holds which property is loading
   - `component_name` (String; optional): Holds the name of the component that is loading
-- `nClicks` (Real; optional)
-- `shape` (a value equal to: 'circle', 'round'; optional)
-- `size` (a value equal to: 'small', 'middle', 'large'; optional)
-- `style` (Dict; optional)
-- `target` (String; optional)
-- `type` (a value equal to: 'primary', 'ghost', 'dashed', 'link', 'text', 'default'; optional)
+- `nClicks` (Real; optional): Recording the number of times the button has been clicked since rendering, default is 0
+- `shape` (a value equal to: 'circle', 'round'; optional): Setting the shape of the button (circle: circle, round: rounded rectangle; default is not set, i.e., normal rectangle)
+- `size` (a value equal to: 'small', 'middle', 'large'; optional): Setting the size of the button, available options are 'small', 'middle', and 'large'; default is 'middle'
+- `style` (Dict; optional): CSS style dict of current component
+- `target` (String; optional): Used in conjunction with the href parameter to set the type of action for the redirection
+- `type` (a value equal to: 'primary', 'ghost', 'dashed', 'link', 'text', 'default'; optional): Setting the overall style of the button (optional options are primary, ghost, dashed, link, text, default; default is default)
 """
 function 'feffery'_antdbutton(; kwargs...)
         available_props = Symbol[:children, :id, :autoSpin, :block, :className, :danger, :debounceWait, :disabled, :href, :icon, :key, :loading, :loadingChildren, :loading_state, :nClicks, :shape, :size, :style, :target, :type]
--- src/lib/components/AntdButton.react.js
+++ src/lib/components/AntdButton.react.js
@@ -94,65 +94,123 @@ const AntdButton = (props) => {
 // 定义参数或属性
 AntdButton.propTypes = {
     // 组件id
+    /**
+     * Id of current component
+     */
     id: PropTypes.string,
 
     // 设置按钮内嵌元素内容
+    /**
+     * Children of current component
+     */
     children: PropTypes.node,
 
     // 设置loading状态下按钮内嵌元素内容
+    /**
+     * Children of current component while loading=true
+     */
     loadingChildren: PropTypes.node,
 
     // css类名
+    /**
+     * CSS class of current component
+     */
     className: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.object
     ]),
 
     // 自定义css字典
+    /**
+     * CSS style dict of current component
+     */
     style: PropTypes.object,
 
     // 辅助刷新用唯一标识key值
+    /**
+     * Refreshing the key can force a redraw of the current component.
+     */
     key: PropTypes.string,
 
     // 设置按钮整体风格（可选项有primary、ghost、dashed、link、text、default，默认为default）
+    /**
+     * Setting the overall style of the button (optional options are primary, ghost, dashed, link, text, default; default is default)
+     */
     type: PropTypes.oneOf(['primary', 'ghost', 'dashed', 'link', 'text', 'default']),
 
     // 设置跳转网址，功能同a标签
+    /**
+     * Setting the URL for redirection, similar to the functionality of an <a> tag
+     */
     href: PropTypes.string,
 
     // 配合href参数使用，设置跳转的动作类型
+    /**
+     * Used in conjunction with the href parameter to set the type of action for the redirection
+     */
     target: PropTypes.string,
 
     // 设置按钮是否撑满父级元素的宽度，默认为false
+    /**
+     * Setting whether the button should span the full width of its parent element, default is false
+     */
     block: PropTypes.bool,
 
     // 设置按钮是否显示为危险状态，默认为false
+    /**
+     * Setting whether the button should be in a dangerous state, default is false
+     */
     danger: PropTypes.bool,
 
     // 设置按钮是否以失效状态渲染，默认为false
+    /**
+     * Setting whether the button should be rendered as a disabled state, default is false
+     */
     disabled: PropTypes.bool,
 
     // 设置按钮形状（circle：圆形，round：圆角矩形，默认不设置，即正常矩形）
+    /**
+     * Setting the shape of the button (circle: circle, round: rounded rectangle; default is not set, i.e., normal rectangle)
+     */
     shape: PropTypes.oneOf(['circle', 'round']),
 
     // 设置按钮大小尺寸，可选的有'small'、'middle'和'large'，默认为'middle'
+    /**
+     * Setting the size of the button, available options are 'small', 'middle', and 'large'; default is 'middle'
+     */
     size: PropTypes.oneOf(['small', 'middle', 'large']),
 
     // 记录按钮从渲染后开始被点击的次数，默认为0
+    /**
+     * Recording the number of times the button has been clicked since rendering, default is 0
+     */
     nClicks: PropTypes.number,
 
     // 用于设置nClicks监听更新的防抖等待时长（单位：毫秒），默认为0
+    /**
+     * Used to set the debounce waiting duration (in milliseconds) for nClicks listener update; default is 0
+     */
     debounceWait: PropTypes.number,
 
     // 组件型参数，设置按钮前缀图标元素
+    /**
+     * Component-specific parameter for setting the prefix icon element of the button
+     */
     icon: PropTypes.node,
 
     // 用于设置是否为按钮渲染“加载中不可点击”效果，默认为false
+    /**
+     * Used to set whether the button renders a "loading, not clickable" effect; default is false
+     */
     loading: PropTypes.bool,
 
     // 设置是否在每次按钮点击之后，是否自动更新loading=true，从而配合回调
     // 实现回调运作中按钮无可点击的效果
     // 默认为false
+    /**
+     * Setting whether to automatically update loading to true after each button click, 
+     * enabling callback operations with a non-clickable button effect; default is false
+     */
     autoSpin: PropTypes.bool,
 
     loading_state: PropTypes.shape({
