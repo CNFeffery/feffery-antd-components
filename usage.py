86210e42cd38cf1696b7b7d9da896d35b31db4ac
CNFeffery <fefferypzy@gmail.com>
2023-06-19 16:22:51 +0800

    tests: 完成部分组件前缀图标快捷参数icon新增fontawesome渲染模式下的测试 #87 #88

--- usage.py
+++ usage.py
@@ -19,6 +19,62 @@ app.layout = html.Div(
                         {
                             'title': '四川省',
                             'key': '四川省',
+                            'children': [
+                                {
+                                    'title': '成都市',
+                                    'key': '成都市',
+                                },
+                                {
+                                    'title': '广安市',
+                                    'key': '广安市',
+                                }
+                            ],
+                            'contextMenu': [
+                                {
+                                    'key': f'四川省-操作选项{i}',
+                                    'label': f'操作选项{i}'
+                                }
+                                for i in range(1, 6)
+                            ]
+                        },
+                        {
+                            'title': '重庆市',
+                            'key': '重庆市',
+                            'children': [
+                                {
+                                    'title': '渝中区',
+                                    'key': '渝中区',
+                                    'children': [
+                                        {
+                                            'title': '解放碑街道',
+                                            'key': '解放碑街道',
+                                        }
+                                    ]
+                                },
+                                {
+                                    'title': '渝北区',
+                                    'key': '渝北区',
+                                }
+                            ],
+                            'contextMenu': [
+                                {
+                                    'key': f'重庆市-操作选项{i}',
+                                    'label': f'操作选项{i}',
+                                    'icon': 'fa-solid fa-cube',
+                                    'iconRenderer': 'fontawesome'
+                                }
+                                for i in range(1, 6)
+                            ]
+                        }
+                    ],
+                    defaultExpandAll=True
+                ),
+
+                fac.AntdTree(
+                    treeData=[
+                        {
+                            'title': '四川省',
+                            'key': '四川省',
                             'icon': 'fa-solid fa-cube',
                             'iconRenderer': 'fontawesome',
                             'children': [
