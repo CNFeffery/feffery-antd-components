0b139452173fcdab23f5baf53ce34b3928be2701
CNFeffery <fefferypzy@gmail.com>
2023-11-01 22:44:30 +0800

    chore: 完成AntdPageHeader的兼容保留

--- usage.py
+++ usage.py
@@ -1,25 +1,51 @@
 import dash
 from dash import html
 import feffery_antd_components as fac
+from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        fac.AntdComment(
-            authorName='费弗里',
-            authorNameHref='https://github.com/CNFeffery/feffery-antd-components',
-            publishTime={
-                'value': '2022-01-01 19:29:01',
-                'format': 'YYYY-MM-DD hh:mm:ss'
-            },
-            commentContent='我希望feffery-components项目系列组件可以帮助更多人快速开发心仪的应用！😀'
+        fac.AntdPageHeader(
+            fac.AntdSpace(
+                [
+                    fac.AntdButton('按钮1'),
+                    fac.AntdButton('按钮2', type='primary'),
+                    fac.AntdSwitch(
+                        checkedChildren='打开',
+                        unCheckedChildren='关闭'
+                    ),
+                    fac.AntdBadge(status='processing')
+                ]
+            ),
+            title='页头标题示例',
+            subTitle='页头副标题示例'
+        ),
+
+        fac.AntdPageHeader(
+            id='page-header-demo',
+            title='页头标题示例',
+            subTitle='页头副标题示例',
+            historyBackDisabled=True
         )
     ],
     style={
         'padding': '50px 100px'
     }
 )
+
+
+@app.callback(
+    Output('page-header-demo', 'children'),
+    Input('page-header-demo', 'backClicks')
+)
+def page_header_demo_callback(backClicks):
+    return [
+        fac.AntdText('backClicks: ', strong=True),
+        fac.AntdText(backClicks)
+    ]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
