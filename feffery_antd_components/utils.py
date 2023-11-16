27fadbfeeb7c67a520b4fc1ab954fc6ddc74fca1
CNFeffery <fefferypzy@gmail.com>
2023-11-16 22:08:14 +0800

    feat: 工具子模块初始化函数df2table

--- feffery_antd_components/utils.py
+++ feffery_antd_components/utils.py
@@ -1,8 +1,25 @@
 import dash
+import warnings
+import importlib
 from collections import defaultdict
-from typing import Union, Literal, Any
+import feffery_antd_components as fac
+from typing import Union, Literal, Any, List
 
-__all__ = ['TreeUtils', 'fill_output_dict']
+try:
+    import pandas as pd
+except ImportError:
+    warning_message = (
+        "检测到您的环境中未安装\033[93mpandas\033[0m，"
+        "部分工具函数如\033[93mdf2table\033[0m将无法正常使用，"
+        "如有需要请使用pip或conda安装pandas。"
+        "\n\nDetected that \033[93mpandas\033[0m is not installed in your environment. "
+        "Some utility functions, such as \033[93mdf2table\033[0m, will not function properly. "
+        "If necessary, please install pandas using pip or conda."
+    )
+    warnings.warn(warning_message, UserWarning)
+
+
+__all__ = ['TreeUtils', 'fill_output_dict', 'df2table']
 
 
 class TreeUtils:
@@ -191,6 +208,98 @@ def fill_output_dict(output_dict: dict, fill_value: Any = dash.no_update) -> dic
         # 通过上下文遍历所有Output字典键名
         for key in dash.ctx.outputs_grouping.keys()
     }
+
+
+def df2table(
+    raw_df: Any,
+    # 样式相关参数
+    # 列宽分配策略，可选的有'auto'、'fit-title'、'equal'
+    column_width_mode: Literal['auto', 'fit-title', 'equal'] = 'auto',
+    bordered: bool = False,  # 表格是否添加框线
+    style: dict = None,  # 表格css样式
+    className: Union[str, dict] = None,  # 表格css类名
+    # 字段筛选功能相关参数
+    str_auto_filter: bool = True,  # 是否针对数据框中的字符型字段自动添加筛选功能
+    str_max_unique_value_count: int = 20,  # 允许自动添加筛选功能的字符型字段唯一值数量上限
+    checkbox_filter_radio_fields: List[str] = None,  # 需要将筛选功能设置为单选模式的字段名数组
+    checkbox_filter_enable_search: bool = True,  # 是否为字段筛选菜单启用搜索框
+) -> fac.AntdTable:
+    """
+    将 pandas DataFrame 转换为 AntdTable 组件。
+
+    Args:
+        raw_df (Any): 要转换的输入 pandas DataFrame。
+        column_width_mode (Literal['auto', 'fit-title', 'equal'], optional): 
+            列宽分配策略，可选值为 'auto'、'fit-title'、'equal'。默认为 'auto'。
+        bordered (bool, optional): 是否为表格添加边框。默认为 False。
+        style (dict, optional): 表格的 CSS 样式。默认为 None。
+        className (Union[str, dict], optional): 表格的 CSS 类名。默认为 None。
+        str_auto_filter (bool, optional): 是否自动为字符串列添加筛选功能。默认为 True。
+        str_max_unique_value_count (int, optional): 自动为字符串列添加筛选功能的唯一值的最大数量。默认为 20。
+        checkbox_filter_radio_fields (List[str], optional): 需要将筛选功能设置为单选模式的字段名数组。默认为 None。
+        checkbox_filter_enable_search (bool, optional): 是否在筛选菜单中启用搜索框。默认为 True。
+
+    Returns:
+        fac.AntdTable: 从输入 DataFrame 生成的 AntdTable 组件。
+    """
+
+    # 默认参数定义
+    checkbox_filter_radio_fields = checkbox_filter_radio_fields or []
+
+    # 数据预处理
+    output_df = pd.DataFrame(raw_df).copy(deep=True)
+
+    # 构造必选参数
+    # 构造columns参数
+    columns = []
+    for column in raw_df.columns:
+        columns.append(
+            {
+                'dataIndex': column,
+                'title': column
+            }
+        )
+    # 根据column_width_mode构造列宽
+    if column_width_mode == 'fit-title':
+        # 计算所有字段名字符数之和
+        columns_title_length_sum = sum(raw_df.columns.map(len))
+        for i, column in enumerate(columns):
+            columns[i]['width'] = 'calc(100% * {} )'.format(
+                len(column['title']) / columns_title_length_sum
+            )
+    elif column_width_mode == 'equal':
+        for i, column in enumerate(columns):
+            columns[i]['width'] = 'calc(100% / {})'.format(len(columns))
+
+    # 构造可选参数
+    optional_params = {}
+    # 构造filterOptions参数
+    if str_auto_filter:
+        filterOptions = {}
+        for column in output_df.select_dtypes(include='object'):
+            # 检查当前字符型字段唯一值数量是否小于等于str_max_unique_value_count
+            if output_df[column].nunique() <= str_max_unique_value_count:
+                filterOptions[column] = {
+                    'filterSearch': checkbox_filter_enable_search
+                }
+                # 检查当前字段是否需要设置为单选模式
+                if column in checkbox_filter_radio_fields:
+                    filterOptions[column]['filterMultiple'] = False
+            else:
+                filterOptions[column] = {
+                    'filterMode': 'keyword'
+                }
+        # 更新到optional_params中
+        optional_params['filterOptions'] = filterOptions
+
+    return fac.AntdTable(
+        data=output_df.to_dict('records'),
+        columns=columns,
+        bordered=bordered,
+        style=style,
+        className=className,
+        **optional_params
+    )
 
 
 if __name__ == '__main__':
--- usage.py
+++ usage.py
@@ -1,79 +1,32 @@
 import dash
-from feffery_antd_components.utils import fill_output_dict
+import numpy as np
+import pandas as pd
+from dash import html
 import feffery_antd_components as fac
-from dash.dependencies import Input, Output, State
+from feffery_antd_components.utils import df2table
 
 app = dash.Dash(__name__)
 
-app.layout = fac.AntdSpace(
+demo_df = pd.DataFrame(
+    np.random.rand(100, 5),
+    columns=[f'字段{str(i)*i}' for i in range(1, 6)]
+)
+
+demo_df['字段6'] = np.random.choice(list('abcdefg'), 100, replace=True)
+demo_df['字段7'] = np.random.choice(list('ABCEDFGHIJKLMNOPKISTUVWXYZ'), 100, replace=True)
+
+app.layout = html.Div(
     [
-        fac.AntdSpace(
-            [
-                fac.AntdButton(
-                    '按钮1',
-                    type='primary',
-                    id='demo-button1'
-                ),
-                fac.AntdButton(
-                    '按钮2',
-                    type='primary',
-                    id='demo-button2'
-                ),
-                fac.AntdInput(
-                    id='demo-input1',
-                    placeholder='输入框1',
-                    style={
-                        'width': 150
-                    }
-                ),
-                fac.AntdInput(
-                    id='demo-input2',
-                    placeholder='输入框2',
-                    style={
-                        'width': 150
-                    }
-                )
-            ]
-        ),
-        fac.AntdSpace(
-            [
-                fac.AntdText(id='demo-output1'),
-                fac.AntdText(id='demo-output2')
-            ]
+        df2table(
+            demo_df,
+            bordered=True,
+            column_width_mode='equal'
         )
     ],
-    direction='vertical',
     style={
-        'padding': 50
+        'padding': '50px 100px'
     }
 )
-
-
-@app.callback(
-    output=dict(
-        content1=Output('demo-output1', 'children'),
-        content2=Output('demo-output2', 'children')
-    ),
-    inputs=dict(
-        nClicks1=Input('demo-button1', 'nClicks'),
-        nClicks2=Input('demo-button2', 'nClicks')
-    ),
-    state=dict(
-        value1=State('demo-input1', 'value'),
-        value2=State('demo-input2', 'value')
-    ),
-    prevent_initial_call=True
-)
-def demo_callback(nClicks1, nClicks2, value1, value2):
-    '''字典化Output配合defaultdict'''
-
-    return fill_output_dict(
-        dict(
-            content1=f'nClicks1: {nClicks1}, nClicks2: {nClicks2}'
-        ),
-        '缺省值示例'
-    )
-
 
 if __name__ == '__main__':
     app.run(debug=True)
