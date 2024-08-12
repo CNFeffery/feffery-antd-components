9dfd4d8e4e48aeb0f3301c3a8196f4b2fe225ec1
CNFeffery <fefferypzy@gmail.com>
2024-08-12 09:28:11 +0800

    chore: 提升antd依赖至^5.20.1，消除AntdSpin部分动画样式问题

--- usage.py
+++ usage.py
@@ -7,53 +7,16 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        fac.AntdSpace(
-            [
-                fac.AntdTable(
-                    id='table',
-                    columns=[
-                        {
-                            'title': f'字段{i}',
-                            'dataIndex': f'字段{i}',
-                            'width': '20%',
-                        }
-                        for i in range(1, 6)
-                    ],
-                    data=[
-                        {
-                            f'字段{i}': '示例内容'
-                            for i in range(1, 6)
-                        }
-                    ]
-                    * 5,
-                    bordered=True,
-                    rowSelectionType='checkbox',
-                    summaryRowContents=[
-                        {
-                            'content': fac.AntdText(
-                                '第1列总结', strong=True
-                            ),
-                            'align': 'center',
-                        },
-                        {
-                            'content': fac.AntdText(
-                                '第2到4列总结', strong=True
-                            ),
-                            'colSpan': 3,
-                            'align': 'center',
-                        },
-                        {
-                            'content': fac.AntdText(
-                                '第5列总结', strong=True
-                            ),
-                            'align': 'center',
-                        },
-                    ],
-                    summaryRowBlankColumns=1,
-                ),
-            ],
-            direction='vertical',
-            style=style(width='100%'),
+        fac.AntdSpin(
+            fac.AntdAlert(
+                message='测试',
+                description='测试',
+                type='info',
+                showIcon=True,
+            ),
+            manual=True,
+            spinning=True,
+            percent='auto',
         )
     ],
     style=style(padding=100),
