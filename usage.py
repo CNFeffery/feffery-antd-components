2dcb1b2b62a260f87fc3cbbd5cbef1b3fc92e6f9
CNFeffery <fefferypzy@gmail.com>
2024-08-22 23:26:33 +0800

    chore: 消除.map产物异常请求影响

--- usage.py
+++ usage.py
@@ -6,50 +6,10 @@ from feffery_dash_utils.style_utils import style
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    [
-        fac.AntdTree(
-            switcherIcon=fac.AntdIcon(icon='antd-down'),
-            treeData=[
-                {
-                    'title': '四川省',
-                    'key': '四川省',
-                    'children': [
-                        {
-                            'title': '成都市',
-                            'key': '成都市',
-                        },
-                        {
-                            'title': '广安市',
-                            'key': '广安市',
-                        },
-                    ],
-                },
-                {
-                    'title': '重庆市',
-                    'key': '重庆市',
-                    'children': [
-                        {
-                            'title': '渝中区',
-                            'key': '渝中区',
-                            'children': [
-                                {
-                                    'title': '解放碑街道',
-                                    'key': '解放碑街道',
-                                }
-                            ],
-                        },
-                        {
-                            'title': '渝北区',
-                            'key': '渝北区',
-                        },
-                    ],
-                },
-            ],
-        )
-    ],
+    [fac.AntdButton('按钮示例')],
     style=style(padding=50),
 )
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
