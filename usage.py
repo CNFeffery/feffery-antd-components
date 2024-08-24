20612d12a8dd6a4e9790e5a80e3e6aef981ff448
CNFeffery <fefferypzy@gmail.com>
2024-08-24 11:31:51 +0800

    chore&fix: 锁定dayjs依赖版本=>^1.11.13，以修复日期选择相关组件涉及的“周”格式解析错误问题

--- usage.py
+++ usage.py
@@ -6,10 +6,39 @@ from feffery_dash_utils.style_utils import style
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    [fac.AntdButton('按钮示例')],
+    [
+        fac.AntdSpace(
+            [
+                fac.AntdDatePicker(
+                    placeholder='picker="date"',
+                    format='YYYY年MM月DD日',
+                    style={'width': 175},
+                ),
+                fac.AntdDatePicker(
+                    placeholder='picker="week"',
+                    picker='week',
+                    format='YYYY年-第w周',
+                    style={'width': 175},
+                ),
+                fac.AntdDatePicker(
+                    placeholder='picker="month"',
+                    picker='month',
+                    format='YYYY年MM月',
+                    style={'width': 175},
+                ),
+                fac.AntdDatePicker(
+                    placeholder='picker="year"',
+                    picker='year',
+                    format='YYYY年',
+                    style={'width': 175},
+                ),
+            ],
+            direction='vertical',
+        )
+    ],
     style=style(padding=50),
 )
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
