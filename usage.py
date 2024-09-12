97c5c1f2e4c62d234e1ee0bc6bc4a38dfabb0c35
CNFeffery <fefferypzy@gmail.com>
2024-09-12 22:57:58 +0800

    chore: antd -> ^5.20.6

--- usage.py
+++ usage.py
@@ -6,10 +6,21 @@ from feffery_dash_utils.style_utils import style
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    [
-        fac.AntdButton('按钮实例', type='primary'),
-        html.Pre(fac.AntdTable.__doc__),
-    ],
+    fac.AntdSpace(
+        [
+            fac.AntdButton(
+                '按钮示例', type='primary', size=size
+            )
+            for size in ['large', 'middle', 'small']
+        ],
+        align='center',
+        style=style(
+            position='fixed',
+            left='50%',
+            top='50%',
+            transform='translate(-50%, -50%)',
+        ),
+    ),
     style=style(padding=50),
 )
 
