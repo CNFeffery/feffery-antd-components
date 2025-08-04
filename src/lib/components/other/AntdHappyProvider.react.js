4e6779f8df0d57a659d67c67a31b4fdba194db31
CNFeffery <fefferypzy@gmail.com>
2025-08-04 16:57:26 +0800

    chore: 调整部分源码缩进大小

--- src/lib/components/other/AntdHappyProvider.react.js
+++ src/lib/components/other/AntdHappyProvider.react.js
@@ -9,43 +9,43 @@ import { useLoading } from "../utils";
  * 快乐工作特效组件AntdHappyProvider
  */
 const AntdHappyProvider = ({
-  children,
-  disabled = false
+    children,
+    disabled = false
 }) => {
-  return (
-    <HappyProvider disabled={disabled} data-dash-is-loading={useLoading()}>
-      {children}
-    </HappyProvider>
-  );
+    return (
+        <HappyProvider disabled={disabled} data-dash-is-loading={useLoading()}>
+            {children}
+        </HappyProvider>
+    );
 };
 
 AntdHappyProvider.propTypes = {
-  /**
-   * 组件唯一id
-   */
-  id: PropTypes.string,
+    /**
+     * 组件唯一id
+     */
+    id: PropTypes.string,
 
-  /**
-   * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
-   */
-  key: PropTypes.string,
+    /**
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+     */
+    key: PropTypes.string,
 
-  /**
-   * 组件型，内嵌元素
-   */
-  children: PropTypes.node,
+    /**
+     * 组件型，内嵌元素
+     */
+    children: PropTypes.node,
 
-  /**
-   * 是否禁用当前特效
-   * 默认值：`false`
-   */
-  disabled: PropTypes.bool,
+    /**
+     * 是否禁用当前特效
+     * 默认值：`false`
+     */
+    disabled: PropTypes.bool,
 
-  /**
-   * Dash-assigned callback that should be called to report property changes
-   * to Dash, to make them available for callbacks.
-   */
-  setProps: PropTypes.func,
+    /**
+     * Dash-assigned callback that should be called to report property changes
+     * to Dash, to make them available for callbacks.
+     */
+    setProps: PropTypes.func,
 };
 
 export default AntdHappyProvider;
