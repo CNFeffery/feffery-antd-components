820f31b2bab36c787dc6800daf054a609d34be2b
CNFeffery <fefferypzy@gmail.com>
2026-02-04 17:44:15 +0800

    chore: 调整setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
-import io
 import json
+from pathlib import Path
+
 from setuptools import setup
 
-
+here = Path(__file__).parent
 with open('package.json') as f:
     package = json.load(f)
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-package_name = package["name"].replace(" ", "_").replace("-", "_")
+package_name = package['name'].replace(' ', '_').replace('-', '_')
 
 setup(
     name=package_name,
-    version=package["version"],
-    author_email=package['email'],
-    homepage='https://github.com/CNFeffery/feffery-antd-components',
+    version=package['version'],
+    homepage=package['homepage'],
     author=package['author'],
     packages=[package_name],
     include_package_data=True,
-    package_data={
-        'feffery_antd_components': ['*']
-    },
+    package_data={'feffery_antd_components': ['*']},
     license=package['license'],
     description=package.get('description', package_name),
-    long_description=io.open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
-        'dash>=3.1.1',
+        'dash>=3.3.0',
     ],
     classifiers=[
         'Framework :: Dash',
     ],
-    url='https://github.com/CNFeffery/feffery-antd-components'
+    url='https://github.com/CNFeffery/feffery-antd-components',
 )
